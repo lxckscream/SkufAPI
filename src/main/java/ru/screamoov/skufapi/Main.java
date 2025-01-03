d82eb20e060cb32abc784a9a8459e2d60a31c192
screamoov <mityafotografer@gmail.com>
2025-01-03 13:48:37 +0300

    1

--- src/main/java/ru/screamoov/skufapi/Main.java
+++ src/main/java/ru/screamoov/skufapi/Main.java
@@ -1,11 +1,15 @@
 package ru.screamoov.skufapi;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.screamoov.skufapi.logger.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static ru.screamoov.skufapi.colorize.Hex.color;
 
 public final class Main extends JavaPlugin {
     static Main api;
@@ -19,6 +23,30 @@ public final class Main extends JavaPlugin {
                 "SkufAPI",
                 getConfig().getBoolean("log.show-time")
         );
+
+        getCommand("skufapi").setExecutor(((commandSender, command, s, strings) -> {
+            if (strings.length == 0) {
+                List<String> list = color(Arrays.asList(
+                        "&#FB2608&lS&#E82007&lk&#D51905&lu&#C21304&lf&#AE0D03&lA&#9B0601&lP&#880000&lI &8- &fРазработан &#797979l&#727272x&#6B6B6Bc&#656565k&#5E5E5ES&#575757c&#505050r&#4A4A4Ae&#434343a&#3C3C3Cm",
+                        "&fМощная библиотека. Требуется ко всем плагинам с началом на 'Skuf'",
+                        "&8=-=-=-=-=  &7Список команд:  &8=-=-=-=-=",
+                        "&#FB2608/&#F32307s&#EC2107k&#E41E06u&#DC1C06f&#D51905a&#CD1705p&#C51404i &#B60F03p&#AE0D03l&#A70A02u&#9F0802g&#970501i&#900301n&#880000s &8- &fСписок плагинов, подключенных на данный момент к SkufAPI."
+                ));
+                for (String ln : list) {
+                    commandSender.sendMessage(color(ln));
+                }
+                return true;
+            }
+            if (strings.length == 1) {
+                if (strings[0].equalsIgnoreCase("plugins")) {
+                    if (connectedPlugins.isEmpty()) commandSender.sendMessage(color("&#FB2608О&#E82007ш&#D51905и&#C21304б&#AE0D03к&#9B0601а&#880000! &7На данный момент нет плагинов, подключенных к SkufAPI."));
+                    for (ISkufPlugin plugin : connectedPlugins) {
+                        commandSender.sendMessage(color("#FB2608" + plugin.getBukkitPlugin().getName() + "&7, ver - #880000" + plugin.getBukkitPlugin().getDescription().getVersion()));
+                    }
+                }
+            }
+            return true;
+        }));
     }
 
     @Override
@@ -36,8 +64,15 @@ public final class Main extends JavaPlugin {
         return log;
     }
 
+    public static void unregisterPlugin(ISkufPlugin plugin) {
+        connectedPlugins.remove(plugin);
+        getLog().info("Анхукнул плагин " + plugin.getBukkitPlugin().getName());
+        plugin.disable();
+    }
+
     public static void registerPlugin(ISkufPlugin plugin) {
         connectedPlugins.add(plugin);
+        getLog().info("Хукнул плагин " + plugin.getBukkitPlugin().getName());
         plugin.enable();
     }
 
